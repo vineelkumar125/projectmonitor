cf4cf7b7450410ad41000d128e347aaa49cd422d
kandunoorivineelkumar-boop <kandunoorivineelkumar@gmail.com>
2026-09-25 01:27:00 +0530

    Connect frontend to live backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,11 +3,30 @@
 // api.interceptors.request.use(c=>{const token=localStorage.getItem('token');if(token)c.headers.Authorization=`Bearer ${token}`;return c;});
 // export default api;
 
+//
+// import axios from 'axios';
+//
+// const api = axios.create({
+//     //baseURL: 'http://localhost:8080/api'
+//    baseURL = "https://projectmonitor-2.onrender.com/api";
+// });
+//
+// api.interceptors.request.use(config => {
+//     const token = localStorage.getItem('token');
+//
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//
+//     return config;
+// });
+//
+// export default api;
 
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    baseURL: 'https://projectmonitor-2.onrender.com/api'
 });
 
 api.interceptors.request.use(config => {
